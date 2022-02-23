d35ac6bf03802fc23fda1920444441c88910a86c
Andro Ranogajec <Andro.Ranogajec@endava.com>
2022-02-23 19:14:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/alt-text */
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [cat, setCat] = useState({});
+  const [catBuffer, setCatBuffer] = useState([])
+  const [catEffect, setCatEffect] = useState(false)
+
+  useEffect(() => {
+    fetchCat("https://api.thecatapi.com/v1/images/search", "GET");
+  }, [catEffect]);
+
+  async function fetchCat(url, verb) {
+    const response = await fetch(url, {
+      method: verb,
+      headers: {
+        "x-api-key": "88f835a0-2587-4eae-9c7f-cf6a44e58ee0",
+      },
+    });
+    const cat = await response.json();
+    catSetter(cat);
+  }
+
+  function isCat(cat) {
+    return cat === undefined || null ? true : false;
+  }
+
+  function catSetter(cat) {
+    if (isCat(cat)) {
+      return
+    }
+    setCat(cat[0]);
+  }
+
+  function handleClick(){
+     setCatEffect(value => !value)
+   
+  }
+  console.log(cat)
+  function Loading(){
+    return (
+      <div className="Loading">
+        Loading...
+      </div>
+    )
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="Cat-button" onClick={handleClick}>CAT</button>
+        <div className="Cat-container">
+          <img className="Cat-img" src={cat.url} />
+        </div>
+        <Loading/>
       </header>
     </div>
   );
